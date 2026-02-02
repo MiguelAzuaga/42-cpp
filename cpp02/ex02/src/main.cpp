3d36cceb82bee8d99c6065f50225a85527562128
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-02 06:11:06 +0000

    Update 'main.cpp' to include tester and adjust initial setup

--- cpp02/ex02/src/main.cpp
+++ cpp02/ex02/src/main.cpp
@@ -1,17 +1,22 @@
 #include "Fixed.hpp"
+#include <stdexcept>
 #include <iostream>
+
+void tester();
 
 int main( void )
 {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << a/0 << std::endl;
+	// Fixed a;
+	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max( a, b ) << std::endl;
+	tester();
+
 	return 0;
 }

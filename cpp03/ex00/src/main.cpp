07785e259e052cde5496f73dda25024df552dd3d
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-03 19:22:30 +0000

    Enhancement: Add displaystatus utility function for ClapTrap

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -1,24 +1,31 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
+void displaystatus(const ClapTrap n)
+{
+	std::cout << n.getName() << " HP:" << n.getHitPoints();
+	std::cout << " " << n.getName() << " EP:" << n.getEnergyPoints();
+	std::cout << " " << n.getName() << " DMG:" << n.getAttackDamage() << std::endl;
+}
+
 int main(void)
 {
 	ClapTrap c1;
-	std::cout << "c1 HP:" << c1.getHitPoints() << " c1 EP:" << c1.getEnergyPoints() << std::endl;
+	displaystatus(c1);
 	c1.attack("Target dummy");
-	std::cout << "c1 HP:" << c1.getHitPoints() << " c1 EP:" << c1.getEnergyPoints() << std::endl;
+	displaystatus(c1);
 	ClapTrap c2("c2");
-	std::cout << "c2 HP:" << c2.getHitPoints() << " c2 EP:" << c2.getEnergyPoints() << std::endl;
+	displaystatus(c2);
 	c2.takeDamage(5);
-	std::cout << "c2 HP:" << c2.getHitPoints() << " c2 EP:" << c2.getEnergyPoints() << std::endl;
+	displaystatus(c2);
 	ClapTrap c3(c2);
-	std::cout << "c3 HP:" << c3.getHitPoints() << " c3 EP:" << c3.getEnergyPoints() << std::endl;
+	displaystatus(c3);
 	c3.takeDamage(50);
-	std::cout << "c3 HP:" << c3.getHitPoints() << " c3 EP:" << c3.getEnergyPoints() << std::endl;
+	displaystatus(c3);
 	c3.attack("Target dummy");
 	c2.attack("Target dummy");
-	std::cout << "c2 HP:" << c2.getHitPoints() << " c2 EP:" << c2.getEnergyPoints() << std::endl;
+	displaystatus(c3);
 	while (c1.getEnergyPoints() > 0)
 		c1.beRepaired(1);
-	std::cout << "c1 HP:" << c1.getHitPoints() << " c1 EP:" << c1.getEnergyPoints() << std::endl;
+	displaystatus(c1);
 }

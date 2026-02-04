cf209dcfd3ec9acaf4617e5dcfd823c36f137753
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-04 15:11:48 +0000

    Introduce ClapTrap.hpp for ClapTrap class

---
+++ cpp03/ex01/inc/ClapTrap.hpp
@@ -0,0 +1,39 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+# include <ostream>
+
+class ClapTrap
+{
+protected:
+	std::string	_name;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
+
+private:
+
+public:
+	//>OCF
+	ClapTrap(void);
+	ClapTrap(const std::string name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	~ClapTrap(void);
+
+	//>MemFn
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+	//>Getters
+	const std::string& getName(void) const;
+	int getHitPoints(void) const;
+	int getEnergyPoints(void) const;
+	int getAttackDamage(void) const;
+};
+std::ostream& operator<<(std::ostream& out, const ClapTrap& in);
+
+
+#endif // CLAPTRAP_HPP

0fec8201180e2fd7ddb4ba858ae46d0647180a5c
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-04 15:11:48 +0000

    Refactor main.cpp to use ostream overload for ClapTrap objects

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -1,31 +1,29 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
-void displaystatus(const ClapTrap n)
-{
-	std::cout << n.getName() << " HP:" << n.getHitPoints();
-	std::cout << " " << n.getName() << " EP:" << n.getEnergyPoints();
-	std::cout << " " << n.getName() << " DMG:" << n.getAttackDamage() << std::endl;
-}
-
 int main(void)
 {
 	ClapTrap c1;
-	displaystatus(c1);
+	std::cout << c1 << std::endl;
 	c1.attack("Target dummy");
-	displaystatus(c1);
-	ClapTrap c2("c2");
-	displaystatus(c2);
+	std::cout << c1 << std::endl;
+	ClapTrap c2("CT-002");
+	std::cout << c2 << std::endl;
 	c2.takeDamage(5);
-	displaystatus(c2);
+	std::cout << c2 << std::endl;
 	ClapTrap c3(c2);
-	displaystatus(c3);
+	std::cout << c3 << std::endl;
 	c3.takeDamage(50);
-	displaystatus(c3);
+	std::cout << c3 << std::endl;
 	c3.attack("Target dummy");
 	c2.attack("Target dummy");
-	displaystatus(c3);
+	std::cout << c3 << std::endl;
+	ClapTrap c4("CT-004");
+	c4 = c2;
+	std::cout << c3 << std::endl;
 	while (c1.getEnergyPoints() > 0)
 		c1.beRepaired(1);
-	displaystatus(c1);
+	std::cout << c1 << std::endl;
+	std::cout << c2 << std::endl;
+	std::cout << c3 << std::endl;
 }

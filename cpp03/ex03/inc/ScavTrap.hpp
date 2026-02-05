6568125aed609166e3c9d17ada822f1c57b64dfd
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-05 11:52:02 +0000

    Introduce ScavTrap.hpp in cpp03/ex03

---
+++ cpp03/ex03/inc/ScavTrap.hpp
@@ -0,0 +1,31 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScavTrap : virtual public ClapTrap
+{
+protected:
+	enum	// constants used by the class
+	{
+		e_defHP = 100,
+		e_defEP = 50,
+		e_defDMG = 20
+	};
+public:
+	//> Orthodox Canonical Form
+	ScavTrap(void);
+	ScavTrap(const std::string& name);
+	ScavTrap(const ScavTrap& other);
+	ScavTrap& operator=(const ScavTrap& other);
+	~ScavTrap(void);
+
+	//> Member Functions
+	// Override attack
+	void attack(const std::string& target);
+	// Special ability
+	void guardGate();
+};
+
+#endif // SCAVTRAP_HPP

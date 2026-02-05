cd0c0e2dddd1dabb0854c818f77d7ee4252f5988
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-05 11:52:02 +0000

    Introduce FragTrap.hpp in cpp03/ex03

---
+++ cpp03/ex03/inc/FragTrap.hpp
@@ -0,0 +1,29 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <string>
+# include "ClapTrap.hpp"
+
+class FragTrap : virtual public ClapTrap
+{
+protected:
+	enum	// constants used by the class
+	{
+		e_defHP = 100,
+		e_defEP = 100,
+		e_defDMG = 30
+	};
+public:
+	//> Orthodox Canonical Form
+	FragTrap(void);
+	FragTrap(const std::string& name);
+	FragTrap(const FragTrap& other);
+	FragTrap& operator=(const FragTrap& other);
+	~FragTrap(void);
+
+	//> Member Functions
+	// Special ability
+	void highFivesGuys(void);
+};
+
+#endif // FRAGTRAP_HPP

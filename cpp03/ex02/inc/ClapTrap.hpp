f7d64bbf06ec2e32131d40891a9bc171d6cb4d23
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-05 11:52:01 +0000

    Introduce ClapTrap.hpp in cpp03/ex02

---
+++ cpp03/ex02/inc/ClapTrap.hpp
@@ -0,0 +1,44 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <ostream>
+
+class ClapTrap
+{
+protected:
+	std::string	_name;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
+
+	enum	// constants used by the class
+	{
+		e_defHP  = 10,
+		e_defEP  = 10,
+		e_defDMG = 0
+	};
+
+public:
+	//> Orthodox Canonical Form
+	ClapTrap(void);
+	ClapTrap(const std::string name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	virtual ~ClapTrap(void);
+
+	//> Member Functions
+	virtual void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+	//> Getters
+	const std::string& getName(void) const;
+	int getHitPoints(void) const;
+	int getEnergyPoints(void) const;
+	int getAttackDamage(void) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const ClapTrap& in);
+
+#endif // CLAPTRAP_HPP

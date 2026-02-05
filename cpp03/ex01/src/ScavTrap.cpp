407a008504f9f520ad4071c7c6072b4d9b903029
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-05 11:52:01 +0000

    Implement ScavTrap.cpp with updated functionality

--- cpp03/ex01/src/ScavTrap.cpp
+++ cpp03/ex01/src/ScavTrap.cpp
@@ -2,86 +2,84 @@
 #include <iostream>
 
 //> ANSI color codes
+
 static const char* C_RST = "\033[0m";
-static const char* C_RED = "\033[31m";
-static const char* C_GRN = "\033[93m";
-static const char* C_YEL = "\033[33m";
-static const char* C_CYN = "\033[36m";
+static const char* C_BOT = "\033[33m"; // Color of the bot: Yellow
 static const char* C_MAG = "\033[35m";
 
-//> Default values
-#define DEF_HP 100
-#define DEF_EP 50
-#define DEF_DMG 20
+//> Orthodox Canonical Form
 
-//> OCF
-ScavTrap::ScavTrap(void) :ClapTrap()
+ScavTrap::ScavTrap(void) : ClapTrap()
 {
-	_hitPoints = DEF_HP;
-	_energyPoints = DEF_EP;
-	_attackDamage = DEF_DMG;
-	std::cout << C_YEL << "ScavTrap " << this->_name
-			  << " has been " << C_MAG << "created!" << C_RST << C_YEL
-			  << "(Default constructor)" << C_RST << std::endl;
+	_hitPoints = e_defHP;
+	_energyPoints = e_defEP;
+	_attackDamage = e_defDMG;
 
+	std::cout << C_BOT << "ScavTrap " << this->_name
+			  << " has been " << C_MAG << "created!" << C_RST
+			  << C_BOT << " (Default constructor)" << C_RST << std::endl;
 }
 
-ScavTrap::ScavTrap(const std::string& name) :ClapTrap(name)
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
 {
-	_hitPoints = DEF_HP;
-	_energyPoints = DEF_EP;
-	_attackDamage = DEF_DMG;
+	_hitPoints = e_defHP;
+	_energyPoints = e_defEP;
+	_attackDamage = e_defDMG;
 
-	std::cout << C_YEL << "ScavTrap " << this->_name
+	std::cout << C_BOT << "ScavTrap " << this->_name
 			  << " has been " << C_MAG << "created!" << C_RST << std::endl;
 }
 
 ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
 {
-	std::cout << C_YEL << "ScavTrap " << this->_name
-			  << " has been " << C_MAG << "created!" << C_RST << C_YEL
-			  << "(Copy of" + other.getName() + ")" << C_RST << std::endl;
+	std::cout << C_BOT << "ScavTrap " << this->_name
+			  << " has been " << C_MAG << "created!" << C_RST
+			  << C_BOT << " (Copy of " << other.getName() << ")" << C_RST << std::endl;
 }
 
 ScavTrap& ScavTrap::operator=(const ScavTrap& other)
 {
-	std::cout << C_YEL << "ScavTrap " << this->_name
-			  << " is now equal to ScavTrap "
-			  << other._name << C_RST << std::endl;
-	this->ClapTrap::operator=(other);
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+
+		std::cout << C_BOT << "ScavTrap " << this->_name
+				  << " is now equal to ScavTrap " << other.getName() << C_RST << std::endl;
+	}
 	return *this;
 }
 
-ScavTrap::~ScavTrap()
+ScavTrap::~ScavTrap(void)
 {
-	std::cout << C_YEL << "ScavTrap " << this->_name
-			  << " has been " << C_MAG << "destroyed!" << C_RST
-			  << std::endl;
+	std::cout << C_BOT << "ScavTrap " << this->_name
+			  << " has been " << C_MAG << "destroyed!" << C_RST << std::endl;
 }
+
+//> Member Functions
 
 void ScavTrap::attack(const std::string& target)
 {
-	if (this->_hitPoints == 0)
+	if (this->_hitPoints <= 0)
 	{
-		std::cout << C_YEL << "ScavTrap " << this->_name
+		std::cout << C_BOT << "ScavTrap " << this->_name
 				  << " is dead and cannot attack. 💀" << C_RST << std::endl;
 		return;
 	}
-	if (this->_energyPoints == 0)
+	if (this->_energyPoints <= 0)
 	{
-		std::cout << C_YEL << "Scavtrap " << this->_name
+		std::cout << C_BOT << "ScavTrap " << this->_name
 				  << " is out of energy and needs to rest" << C_RST << std::endl;
 		return;
 	}
 	this->_energyPoints--;
-	std::cout << C_YEL << "ScavTrap " << _name
-			  << " fiercely attacks " << C_CYN << target << C_RST
-			  << C_YEL << ", dealing " << C_MAG << _attackDamage << C_RST
-			  << C_YEL << " points of damage!" << C_RST << std::endl;
+	std::cout << C_BOT << "ScavTrap " << this->_name
+			  << " fiercely attacks " << C_MAG << target << C_RST
+			  << C_BOT << ", dealing " << C_MAG << this->_attackDamage << C_RST
+			  << C_BOT << " points of damage!" << C_RST << std::endl;
 }
 
 void ScavTrap::guardGate()
 {
-	std::cout << C_YEL << "ScavTrap " << _name
+	std::cout << C_BOT << "ScavTrap " << this->_name
 			  << " is now in Gate keeper mode!" << C_RST << std::endl;
 }

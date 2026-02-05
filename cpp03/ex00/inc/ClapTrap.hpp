355f38dcdad7e7eb11607bcba3fa878ff232d190
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-05 11:52:01 +0000

    Fix and reformat ClapTrap.hpp

--- cpp03/ex00/inc/ClapTrap.hpp
+++ cpp03/ex00/inc/ClapTrap.hpp
@@ -1,8 +1,8 @@
 #ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
-# include <string>
-# include <ostream>
+#include <string>
+#include <ostream>
 
 class ClapTrap
 {
@@ -12,26 +12,33 @@ private:
 	int			_energyPoints;
 	int			_attackDamage;
 
+	enum	// constants used by the class
+	{
+		e_defHP  = 10,
+		e_defEP  = 10,
+		e_defDMG = 0
+	};
+
 public:
-	//>OCF
+	//> Orthodox Canonical Form
 	ClapTrap(void);
 	ClapTrap(const std::string name);
 	ClapTrap(const ClapTrap& other);
 	ClapTrap& operator=(const ClapTrap& other);
 	~ClapTrap(void);
 
-	//>MemFn
+	//> Member Functions
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
 
-	//>Getters
+	//> Getters
 	const std::string& getName(void) const;
 	int getHitPoints(void) const;
 	int getEnergyPoints(void) const;
 	int getAttackDamage(void) const;
 };
-std::ostream& operator<<(std::ostream& out, const ClapTrap& in);
 
+std::ostream& operator<<(std::ostream& out, const ClapTrap& in);
 
 #endif // CLAPTRAP_HPP

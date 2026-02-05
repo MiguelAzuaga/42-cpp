f2587074675c1c1a6f09c674021f7488c0be9854
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-05 11:52:01 +0000

    Enhance ScavTrap.hpp to include constants and formatting

--- cpp03/ex01/inc/ScavTrap.hpp
+++ cpp03/ex01/inc/ScavTrap.hpp
@@ -4,16 +4,24 @@
 # include <string>
 # include "ClapTrap.hpp"
 
-class ScavTrap : public ClapTrap {
+class ScavTrap : public ClapTrap
+{
+protected:
+	enum	// constants used by the class
+	{
+		e_defHP = 100,
+		e_defEP = 50,
+		e_defDMG = 20
+	};
 public:
-	//>OCF
+	//> Orthodox Canonical Form
 	ScavTrap(void);
 	ScavTrap(const std::string& name);
 	ScavTrap(const ScavTrap& other);
 	ScavTrap& operator=(const ScavTrap& other);
 	~ScavTrap(void);
 
-	//>MemFn
+	//> Member Functions
 	// Override attack
 	void attack(const std::string& target);
 	// Special ability

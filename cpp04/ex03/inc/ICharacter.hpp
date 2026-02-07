318f1fab824a46dcf116ed0a6224c7c8b01e9433
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-07 17:12:03 +0000

    Introduce ICharacter interface

---
+++ cpp04/ex03/inc/ICharacter.hpp
@@ -0,0 +1,22 @@
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include <string>
+
+class AMateria;
+
+class ICharacter
+{
+public:
+	virtual ~ICharacter() {}
+
+	//> Member Functions
+	virtual void equip(AMateria* m) = 0;
+	virtual void unequip(int idx) = 0;
+	virtual void use(int idx, ICharacter& target) = 0;
+
+	//> Getters
+	virtual std::string const& getName() const = 0;
+};
+
+#endif // ICHARACTER_HPP

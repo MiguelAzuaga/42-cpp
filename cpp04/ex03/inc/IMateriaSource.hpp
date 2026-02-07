80bdf5d8a62f5efa72ce5e32e4e69075a361b5e3
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-07 17:12:03 +0000

    Introduce IMateriaSource interface

---
+++ cpp04/ex03/inc/IMateriaSource.hpp
@@ -0,0 +1,21 @@
+#ifndef IMATERIASOURCE_HPP
+#define IMATERIASOURCE_HPP
+
+#include <string>
+
+class AMateria;
+
+class IMateriaSource
+{
+private:
+	std::string _name;
+
+public:
+	virtual ~IMateriaSource() {}
+
+	//> Member Functions
+	virtual void learnMateria(AMateria*) = 0;
+	virtual AMateria* createMateria(const std::string& type) = 0;
+};
+
+#endif // IMATERIASOURCE_HPP

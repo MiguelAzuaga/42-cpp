66a48317d4dabe7ed2811c53ba3ec751b9eaee84
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2026-02-09 14:36:02 +0000

    Add Bureaucrat.hpp header file with class definition

---
+++ cpp05/ex00/inc/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+#define MIN_GRADE 150
+#define MAX_GRADE 1
+
+class Bureaucrat
+{
+private:
+	const std::string _name;
+	int _grade;
+
+public:
+	//> Orthodox Canonical Form
+	Bureaucrat();
+	Bureaucrat(const std::string name, int grade);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
+
+	//> Exceptions
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+
+	//> Member Functions
+	void incrementGrade();
+	void decrementGrade();
+
+	//> Getters
+	const std::string& getName() const;
+	int getGrade() const;
+};
+
+//> Stream
+std::ostream& operator<<(std::ostream& out, const Bureaucrat in);
+
+#endif // BUREAUCRAT_HPP
